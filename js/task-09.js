456fc32aba5b7536f705a4dec0a4170c60a7068d
VitalikApter <aptvetal2014@gmail.com>
2022-11-05 20:59:26 +0200

    9
    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const body = document.body;
+const btnChangeColor = body.querySelector('.change-color');
+const span = body.querySelector('.color');
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnChangeColor.addEventListener('click', onClickBtnChangeColor);
+
+function onClickBtnChangeColor (event) {
+  body.style.backgroundColor = getRandomHexColor ();
+  span.textContent = body.style.backgroundColor;
 }
